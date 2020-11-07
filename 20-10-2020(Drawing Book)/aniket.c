3fcf12f1370901a13f512f03e279e1cc977ebea6
Aniket-07Sur <72658088+Aniket-07Sur@users.noreply.github.com>
2020-11-07 20:09:36 +0530

    Drawing Book
    Drawing Book

---
+++ 20-10-2020(Drawing Book)/aniket.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int pageCount(int n,int p)
+{
+   if((n/2)>=p)
+    {
+       return (p/2);
+    }
+    else
+    {
+        if(n%2==0)
+            n++;
+        if(p%2==0)
+            p++;
+        return(n-p)/2;
+    }
+}
+int main()
+{
+    int n,p;
+    scanf("%d%d",&n,&p);
+    printf("%d",pageCount(n,p));
+    return 0;
+}
